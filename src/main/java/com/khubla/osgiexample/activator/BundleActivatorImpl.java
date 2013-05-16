4493e7c42bac0445e0f7487b92412d801637330b
Tom Everett <tome@criticalmass.com>
2013-05-16 12:54:50 -0600

    checked in shitty version
    Signed-off-by: Tom Everett <tome@criticalmass.com>

--- src/main/java/com/khubla/osgiexample/activator/BundleActivatorImpl.java
+++ src/main/java/com/khubla/osgiexample/activator/BundleActivatorImpl.java
@@ -1,14 +1,37 @@
 package com.khubla.osgiexample.activator;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import com.khubla.osgiexample.service.HelloService;
 
 /**
  * @author tome
  */
 public class BundleActivatorImpl implements BundleActivator {
+   /**
+    * the registration interface for the hello service
+    */
+   private ServiceRegistration<HelloService> helloServiceRegistration;
+
+   public ServiceRegistration<HelloService> getHelloServiceRegistration() {
+      return helloServiceRegistration;
+   }
+
+   public void setHelloServiceRegistration(ServiceRegistration<HelloService> helloServiceRegistration) {
+      this.helloServiceRegistration = helloServiceRegistration;
+   }
+
+   @SuppressWarnings("unchecked")
    public void start(BundleContext bundleContext) throws Exception {
-      System.out.println("Hello");
+      final Dictionary<String, String> properties = new Hashtable<String, String>();
+      properties.put("Language", "English");
+      helloServiceRegistration = (ServiceRegistration<HelloService>) bundleContext.registerService(HelloService.class.getName(), this, properties);
+      System.out.println("Registered the HelloService");
    }
 
    public void stop(BundleContext bundleContext) throws Exception {
