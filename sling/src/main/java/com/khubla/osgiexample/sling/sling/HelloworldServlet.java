00d4f69f8883ae52df819fbcd418b1e3d39357f7
Tom Everett <tome@criticalmass.com>
2013-06-19 09:56:04 -0600

    pom updates
    Signed-off-by: Tom Everett <tome@criticalmass.com>

--- sling/src/main/java/com/khubla/osgiexample/sling/sling/HelloworldServlet.java
+++ sling/src/main/java/com/khubla/osgiexample/sling/sling/HelloworldServlet.java
@@ -9,18 +9,36 @@ import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 
+import com.khubla.osgiexample.sling.service.HelloService;
+
 /**
  * @author tome
  */
-@SlingServlet(paths = "/foo/bar/my-servlet")
+@SlingServlet(paths = "/hello")
 public class HelloworldServlet extends SlingSafeMethodsServlet {
    /**
     * 
     */
    private static final long serialVersionUID = 1L;
+   /**
+    * the hello service
+    */
+   private static HelloService helloService;
+
+   public static HelloService getHelloService() {
+      return helloService;
+   }
+
+   public static void setHelloService(HelloService helloService) {
+      HelloworldServlet.helloService = helloService;
+   }
 
    @Override
-   protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
-      response.getWriter().println("hello world");
+   protected void doGet(SlingHttpServletRequest slingHttpServletRequest, SlingHttpServletResponse slingHttpServletResponse) throws ServletException, IOException {
+      if (null != helloService) {
+         slingHttpServletResponse.getWriter().println(helloService.sayHello());
+      } else {
+         slingHttpServletResponse.getWriter().println("helloService has not been bound");
+      }
    }
 }
